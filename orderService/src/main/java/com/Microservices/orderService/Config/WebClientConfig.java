cd8806a2eb3d58fd3aaad81671a9aa9d318c3f54
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-12-24 21:50:09 +0530

    complete path readme.md

--- orderService/src/main/java/com/Microservices/orderService/Config/WebClientConfig.java
+++ orderService/src/main/java/com/Microservices/orderService/Config/WebClientConfig.java
@@ -6,9 +6,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configurable
 public class WebClientConfig {
-
+//    The client-side load balancer inside Order Service decides which Inventory instance is called.
     @Bean
     public WebClient webClient(){
         return WebClient.builder().build();
     }
 }
+
+
+/**
+ * Without @LoadBalanced →
+ * inventory-service will not be resolved
+ * */
